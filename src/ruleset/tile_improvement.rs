a12b07cdd1ba3a3986fa634490b51dd32b30db28
lishaoxia1985 <lishaoxia1985@hotmail.com>
2026-05-21 15:00:44 +0800

    Refactor tile improvements to use requiredTerrain structure
    - Updated TileImprovements.json to replace canBeBuiltOnType, canBeBuiltOnBase, and canBeBuiltOnFeature with a unified requiredTerrain field.
    - Introduced RequiredTerrain struct in tile_improvement.rs to encapsulate terrain requirements, including terrain type, base terrain, features, and additional conditions.
    - Enhanced unique properties of tile improvements to include pillaging rewards for various improvements.
    - Adjusted existing improvements to align with the new structure and added new improvements such as Offshore Platform and Polder.

--- src/ruleset/tile_improvement.rs
+++ src/ruleset/tile_improvement.rs
@@ -16,17 +16,14 @@ pub struct TileImprovement {
     #[serde(flatten)]
     pub yields: Yields,
     #[serde(default)]
-    pub can_be_built_on_type: Vec<TerrainType>,
-    #[serde(default)]
-    pub can_be_built_on_base: Vec<BaseTerrain>,
-    #[serde(default)]
-    pub can_be_built_on_feature: Vec<Feature>,
-    #[serde(default)]
-    pub turns_to_build: i8,
+    pub required_terrain: Vec<RequiredTerrain>,
     #[serde(default)]
     pub required_tech: String,
     #[serde(default)]
+    pub turns_to_build: i8,
+    #[serde(default)]
     pub unique_to: String,
+    #[serde(default)]
     pub uniques: Vec<String>,
     pub shortcut_key: Option<char>,
     #[serde(default)]
@@ -37,4 +34,26 @@ impl Name for TileImprovement {
     fn name(&self) -> String {
         self.name.to_owned()
     }
+}
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RequiredTerrain {
+    pub terrain_type: Vec<TerrainType>,
+    pub base_terrain: Vec<BaseTerrain>,
+    /// When it's `None`, it means the required terrain will ignore this value,
+    /// which means it can be any feature or no feature.
+    pub feature: Option<Vec<Feature>>,
+    /// When it's `None`, it means the required terrain will ignore this value,
+    /// which means the required terrain can be freshwater or not.
+    #[serde(default)]
+    pub freshwater: Option<bool>,
+    /// When it's `None`, it means the required terrain will ignore this value,
+    /// which means the required terrain can be coastal land or not.
+    ///
+    /// A tile is considered `coastal` tile if it is not `Water` and has at least one neighboring tile that is `Coast`.
+    #[serde(default)]
+    pub coastal: Option<bool>,
+    #[serde(default)]
+    pub extra_conditions: Vec<String>,
 }
